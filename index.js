957ca5f5acff243c061109f00deda0a3b6cf4f96
ambat25 <25ambat25@gmail.com>
2019-10-19 21:39:15 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const twit = require('twit');
+const config = require('./config');
+const T = new twit(config);
+
+const retweet = id => {
+	T.post('statuses/retweet/:id', { id: `${id}` }, function(err, data, response) {});
+};
+
+const work = () => {
+	T.get('search/tweets', { q: '#devfestkano' }, function(err, data, response) {
+		if (!err && data && data.statuses) {
+			data.statuses.map(d => retweet(d.id_str));
+		}
+	});
+	console.log('working at:', new Date());
+};
+
+setInterval(work, 12000);
